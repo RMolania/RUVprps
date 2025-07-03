2a24b100fc20af9cbd830ab68b3136028ac6fe5f
Ramyar Molania <ramyar.molania@gmail.com>
2025-07-03 10:24:26 -0400

    new function to remove assays in se object

---
+++ R/removeAssays.R
@@ -0,0 +1,21 @@
+#' Removes datasets (assays) from a SummarizedExperiment object
+#'
+#' @param se.obj A SummarizedExperiment object.
+#' @param assays.to.remove Character. A character to a vector of characters that specifies the name of data (assays) that
+#' will be removed from the SummarizedExperiment object.
+#'
+#' @importFrom SummarizedExperiment assays
+#'
+removeAssays <- function(
+        se.obj,
+        assays.to.remove
+        ){
+    # Checking the input ####
+    if (sum(assays.to.remove %in% names(assays(se.obj)))!= length(assays.to.remove)){
+        stop('All or some of the "assays.to.remove" cannot be found in the SummarizedExperiment object.')
+    }
+    for(i in assays.to.remove){
+        se.obj@assays@data[[i]] <- NULL
+    }
+    return(se.obj)
+}
