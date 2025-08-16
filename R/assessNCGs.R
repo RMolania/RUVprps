8429b5d85682c3ca3173b7d8a61fbf143a1f3173
Ramyar Molania <ramyar.molania@gmail.com>
2025-08-16 19:21:59 -0400

    updte the arg and documentation

--- R/assessNCGs.R
+++ R/assessNCGs.R
@@ -34,10 +34,10 @@
 #' variable types. The default is set to `NULL`. This must be specified if the performance scores is need to be calculated.
 #' @param ncg.type Character. A character that specifies the type of NCGs to be selected and assessed their performnce.
 #' Options are `'supervised'`, `'un.supervised'`, `pre.selected` and `all.genes`.
-#' @param ncg.group Character. A character that specifies the name of the group of NCGs stored under the 'supervised' or
+#' @param  ncg.group.name Character. A character that specifies the name of the group of NCGs stored under the 'supervised' or
 #' 'un.supervised' slots in the SummarizedExperiment object. Refer to supervised and unsupervised functions for finding
 #' NCGS for more details.
-#' @param ncg.set.names Character. Specifies the exact name of an NCG set under the given `ncg.group` in either the
+#' @param ncg.set.names Character. Specifies the exact name of an NCG set under the given ` ncg.group.name` in either the
 #' 'supervised' or 'un.supervised' slot or in the `pre.selected` slot. The default is se to `'all'`, which selects all
 #' available NCG sets.
 #' @param nb.pcs Numeric. A numeric value that indicates the number of principal components (PCs) to compute and use for
@@ -83,7 +83,7 @@ assessNCGs <- function(
         bio.variables = NULL,
         uv.variables = NULL,
         ncg.type,
-        ncg.group,
+        ncg.group.name,
         ncg.set.names = 'all',
         nb.pcs = 5,
         center = TRUE,
@@ -103,6 +103,9 @@ assessNCGs <- function(
     }
     if (length(assay.name) > 1 | assay.name == 'all') {
         stop('The "assay.name" must be a name of a data (assay) in the SummarizedExperiment object.')
+    }
+    if (!is.null(bio.variables) & !is.null(uv.variables)){
+        variables <- NULL
     }
     if (!is.null(variables)){
         if (!is.character(variables)){
@@ -155,8 +158,8 @@ assessNCGs <- function(
             stop('All or some of the "uv.variables" cannot be found in the SummarizedExperiment object.')
         }
     }
-    if (is.null(ncg.type) | is.null(ncg.group)){
-        stop('The "ncg.type" and "ncg.group" must be provided.')
+    if (is.null(ncg.type) | is.null( ncg.group.name)){
+        stop('The "ncg.type" and " ncg.group.name" must be provided.')
     }
     if (!ncg.type %in% c('supervised', 'un.supervised', 'pre.selected', 'all.genes')){
         stop('The "ncg.type" must be one of the "supervised", "un.supervised", "pre.selected" or "all.genes".')
@@ -164,7 +167,7 @@ assessNCGs <- function(
     if (!is.character(ncg.type) | length(ncg.type) > 1){
         stop('The "ncg.type" must be a single character.')
     }
-    if (!is.character(ncg.group) | length(ncg.group) > 1){
+    if (!is.character( ncg.group.name) | length( ncg.group.name) > 1){
         stop('The "ncg.type" must be a single character.')
     }
     if (!is.character(ncg.set.names)){
@@ -213,53 +216,87 @@ assessNCGs <- function(
 
     # Obtaining the NCGs sets ####
     printColoredMessage(
-        message = '-- Obtaining specified NCG sets:',
+        message = '-- Obtaining the specified NCG sets:',
         color = 'magenta',
         verbose = verbose
         )
     ## Supervised NCGs selection ####
     if (ncg.type == 'supervised'){
         printColoredMessage(
-            message = '- Obtaining specified NCG sets selected by the supervised approaches:',
+            message = '- Obtaining the specified NCG sets found by the supervised approaches:',
             color = 'blue',
             verbose = verbose
             )
-        if (ncg.set.names == 'all'){
-            ncgs <- se.obj@metadata$NCG$supervised[[ncg.group]]$ncg.set
+        if (sum(ncg.set.names == 'all') == 1){
+            ncg.set.names <- names(se.obj@metadata$NCG$supervised[[ncg.group.name]])
+            ncg.set.names <- ncg.set.names[!ncg.set.names %in% 'assessment.plot']
+            ncgs <- lapply(
+                ncg.set.names,
+                function(x){
+                    se.obj@metadata$NCG$supervised[[ncg.group.name]][[x]]$ncg.set
+                })
+            names(ncgs) <- ncg.set.names
         } else {
-            if (sum(ncg.set.names %in% names(se.obj@metadata$NCG$supervised[[ncg.group]]$ncg.set)) != length(ncg.set.names) ){
+            if (sum(ncg.set.names %in% names(se.obj@metadata$NCG$supervised[[ncg.group.name]])) != length(ncg.set.names)){
                 stop('All or some of the "ncg.set.names" cannot be found in the metadata of the SummarizedExperiment obejct.')
             } else {
                 ncgs <- lapply(
                     ncg.set.names,
                     function(x){
-                        se.obj@metadata$NCG$supervised[[ncg.group]]$ncg.set[[x]]
+                        se.obj@metadata$NCG$supervised[[ncg.group.name]][[x]]$ncg.set
                     })
                 names(ncgs) <- ncg.set.names
             }
         }
+        printColoredMessage(
+            message = paste0(
+                '- The total number of ',
+                length(ncg.set.names),
+                ' NCG sets are obtained:'),
+            color = 'blue',
+            verbose = verbose
+            )
+        print(sapply(ncgs, sum))
+
     }
     ## Un supervised NCGs selection ####
     if (ncg.type == 'un.supervised'){
         printColoredMessage(
-            message = '- Obtaining specified NCG sets selected by the un supervised approaches:',
+            message = '- Obtaining the specified NCG sets found by the un-supervised approaches:',
             color = 'blue',
             verbose = verbose
             )
         if (ncg.set.names == 'all'){
-            ncgs <- se.obj@metadata$NCG$un.supervised[[ncg.group]]$ncg.set
+            ncg.set.names <- names(se.obj@metadata$NCG$un.supervised[[ncg.group.name]])
+            ncg.set.names <- ncg.set.names[!ncg.set.names %in% 'assessment.plot']
+            ncgs <- lapply(
+                ncg.set.names,
+                function(x){
+                    se.obj@metadata$NCG$un.supervised[[ncg.group.name]][[x]]$ncg.set
+                })
+            names(ncgs) <- ncg.set.names
+
         } else {
-            if (sum(ncg.set.names %in% names(se.obj@metadata$NCG$un.supervised[[ncg.group]]$ncg.set)) != length(ncg.set.names) ){
+            if (sum(ncg.set.names %in% names(se.obj@metadata$NCG$un.supervised[[ ncg.group.name]]$ncg.set)) != length(ncg.set.names) ){
                 stop('All or some of the "ncg.set.names" cannot be found in the SummarizedExperiment.')
             } else{
                 ncgs <- lapply(
                     ncg.set.names,
                     function(x){
-                        se.obj@metadata$NCG$un.supervised[[ncg.group]]$ncg.set[[x]]
+                        se.obj@metadata$NCG$un.supervised[[ncg.group.name]][[x]]$ncg.set
                     })
                 names(ncgs) <- ncg.set.names
             }
         }
+        printColoredMessage(
+            message = paste0(
+                '- The total number of ',
+                length(ncg.set.names),
+                ' NCG sets are obtained:'),
+            color = 'blue',
+            verbose = verbose
+        )
+        print(sapply(ncgs, sum))
     }
     ## Pre selected sets ####
     if (ncg.type == 'pre.selected'){
@@ -277,7 +314,7 @@ assessNCGs <- function(
                 ncgs <- lapply(
                     ncg.set.names,
                     function(x){
-                        se.obj@metadata$NCG$un.supervised[[ncg.group]]$ncg.set[[x]]
+                        se.obj@metadata$NCG$un.supervised[[ ncg.group.name]]$ncg.set[[x]]
                     })
                 names(ncgs) <- ncg.set.names
             }
@@ -297,7 +334,7 @@ assessNCGs <- function(
         message = '-- Assessing the performance of the specified NCG sets:',
         color = 'magenta',
         verbose = verbose
-    )
+        )
     printColoredMessage(
         message = '- Performing PCA using only selected NCGs:',
         color = 'blue',
@@ -320,28 +357,31 @@ assessNCGs <- function(
                 check.se.obj = FALSE,
                 remove.na = 'none',
                 save.se.obj = FALSE
-            )
+                )
             pcs[[assay.name]]$svd$u
         })
     names(all.pcs.on.ncgs) <- names(ncgs)
     ## Comparing the performance of different NCG sets ####
+    if (!is.null(bio.variables) & !is.null(uv.variables)){
+        variables <- NULL
+    }
     if (is.null(variables)) {
         printColoredMessage(
             message = '- Comparing the performance of different NCG sets:',
             color = 'blue',
             verbose = verbose
-        )
+            )
         #### Finding the class of the variables ####
         printColoredMessage(
             message = '- Finding the class of the variable:',
             color = 'blue',
             verbose = verbose
-        )
+            )
         all.vars <- c(bio.variables, uv.variables)
         class.all.vars <- sapply(
             all.vars,
             function(x) class(colData(x = se.obj)[[x]])
-        )
+            )
         cont.vars <- names(class.all.vars[class.all.vars %in% c('numeric', 'integer')])
         cat.vars <- names(class.all.vars[class.all.vars %in% c('factor', 'character')])
         ## Linear regression analysis ####
@@ -395,18 +435,26 @@ assessNCGs <- function(
             summarise(assess = corr[groups == 'wanted']/2 + corr[groups == 'unwanted']/2) %>%
             arrange(desc(assess)) %>%
             mutate(data = factor(geneset, levels = geneset))
+        wrapped.labels <- gsub("\\.", "\n", levels(all$data))
         p.comparing.ncgs <- ggplot(all, aes(x = data, y = assess)) +
             geom_bar(stat = 'identity', fill = 'grey') +
-            ggtitle('Comparison of the NCG sets') +
+            ylim(c(0, 1)) +
+            ggtitle('') +
+            scale_x_discrete(labels = wrapped.labels) +
             xlab('') +
-            ylab('Correlations') +
+            ylab('Summarized correlations') +
             theme(
                 panel.background = element_blank(),
                 axis.line = element_line(colour = 'black', linewidth = 1),
                 axis.title.x = element_text(size = 18),
                 axis.title.y = element_text(size = 18),
                 plot.title = element_text(size = 15),
-                axis.text.x = element_text(size = 12, angle = 90, hjust = 1),
+                axis.text.x = element_text(
+                    size = 12,
+                    angle = 35,
+                    hjust = 0.9,
+                    vjust = 1
+                ),
                 axis.text.y = element_text(size = 12)
             )
         if (isTRUE(plot.output)) print(p.comparing.ncgs)
@@ -485,34 +533,37 @@ assessNCGs <- function(
         verbose = verbose
         )
     if (is.null(output.name)){
-        output.name <- paste0('comparsion_', paste0(names(ncgs), collapse = '&'))
+        output.name <- paste0(
+            'comparsion_',
+            paste0(names(ncgs), collapse = '&')
+            )
     }
     if (isTRUE(save.se.obj)){
-        if (ncg.group %in% c('supervised', 'un.supervised')){
+        if ( ncg.type %in% c('supervised', 'un.supervised')){
             if (length(se.obj@metadata) == 0) {
                 se.obj@metadata[['NCG']] <- list()
             }
             if (!ncg.type %in% names(se.obj@metadata[['NCG']])){
                 se.obj@metadata[['NCG']][[ncg.type]] <- list()
             }
-            if (!ncg.group %in% names(se.obj@metadata[['NCG']][[ncg.type]])){
-                se.obj@metadata[['NCG']][[ncg.type]][[ncg.group]] <- list()
+            if (! ncg.group.name %in% names(se.obj@metadata[['NCG']][[ncg.type]])){
+                se.obj@metadata[['NCG']][[ncg.type]][[ ncg.group.name]] <- list()
             }
-            if (!'assessment.plot' %in% names(se.obj@metadata[['NCG']][[ncg.type]][[ncg.group]])){
-                se.obj@metadata[['NCG']][[ncg.type]][[ncg.group]][['assessment.plot']] <- list()
+            if (!'assessment.plot' %in% names(se.obj@metadata[['NCG']][[ncg.type]][[ ncg.group.name]])){
+                se.obj@metadata[['NCG']][[ncg.type]][[ ncg.group.name]][['assessment.plot']] <- list()
             }
-            if (!output.name %in% names(se.obj@metadata[['NCG']][[ncg.type]][[ncg.group]][['assessment.plot']] )){
-                se.obj@metadata[['NCG']][[ncg.type]][[ncg.group]][['assessment.plot']][[output.name]] <- list()
+            if (!output.name %in% names(se.obj@metadata[['NCG']][[ncg.type]][[ ncg.group.name]][['assessment.plot']] )){
+                se.obj@metadata[['NCG']][[ncg.type]][[ ncg.group.name]][['assessment.plot']][[output.name]] <- list()
             }
             if (!is.null(p.assess.ncg)){
-                se.obj@metadata[['NCG']][[ncg.type]][[ncg.group]][['assessment.plot']][[output.name]]<- p.assess.ncg
+                se.obj@metadata[['NCG']][[ncg.type]][[ ncg.group.name]][['assessment.plot']][[output.name]]<- p.assess.ncg
             }
             if (!is.null(p.comparing.ncgs)){
-                se.obj@metadata[['NCG']][[ncg.type]][[ncg.group]][['assessment.plot']][[output.name]]<- p.comparing.ncgs
+                se.obj@metadata[['NCG']][[ncg.type]][[ncg.group.name]][['assessment.plot']][[output.name]]<- p.comparing.ncgs
             }
             return(se.obj)
         }
-        if (ncg.group == 'pre.selected'){
+        if ( ncg.group.name == 'pre.selected'){
             if (length(se.obj@metadata$NCG) == 0 ) {
                 se.obj@metadata[['NCG']] <- list()
             }
@@ -526,14 +577,14 @@ assessNCGs <- function(
                 se.obj@metadata[['NCG']][['pre.selected']][['gene.set']][['assessment.plot']][[output.name]] <- list()
             }
             if (!is.null(p.assess.ncg)){
-                se.obj@metadata[['NCG']][[ncg.type]][[ncg.group]][['assessment.plot']][[output.name]]<- p.assess.ncg
+                se.obj@metadata[['NCG']][[ncg.type]][[ ncg.group.name]][['assessment.plot']][[output.name]]<- p.assess.ncg
             }
             if (!is.null(p.comparing.ncgs)){
-                se.obj@metadata[['NCG']][[ncg.type]][[ncg.group]][['assessment.plot']][[output.name]]<- p.comparing.ncgs
+                se.obj@metadata[['NCG']][[ncg.type]][[ ncg.group.name]][['assessment.plot']][[output.name]]<- p.comparing.ncgs
             }
             se.obj@metadata[['NCG']][['pre.selected']][['gene.set']][['assessment.plot']][[output.name]] <- p.assess.ncg
         }
-        if (ncg.group == 'all.genes'){
+        if ( ncg.group.name == 'all.genes'){
             if (length(se.obj@metadata$NCG) == 0 ) {
                 se.obj@metadata[['NCG']] <- list()
             }
@@ -553,7 +604,6 @@ assessNCGs <- function(
                 se.obj@metadata[['NCG']][['all.genes']][['assessment.plot']][[output.name]]<- p.comparing.ncgs
             }
         }
-
     }
     if (isFALSE(save.se.obj)){
         return(list(p.assess.ncg = p.assess.ncg))
