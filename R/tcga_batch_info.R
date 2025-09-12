e9747a3d843b3665dff43a8f16b3c8364e55575b
Ramyar Molania <ramyar.molania@gmail.com>
2025-09-12 14:49:15 -0400

    adding infor regarding TCGA batch effect data

---
+++ R/tcga_batch_info.R
@@ -0,0 +1,29 @@
+#' TCGA Batch Information
+#'
+#' This dataset contains batch metadata for TCGA samples used in the RUVprps package.
+#'
+#' @format A data frame with X rows and Y variables:
+#' \describe{
+#'   \item{Sample.ids.full.a}{Unique identifier for each sample}
+#'   \item{Sample.ids.full}{Batch information for each sample}
+#'   \item{Sample.ids.3}{Other metadata columns...}
+#'   \item{Sample.ids.4}{Other metadata columns...}
+#'   \item{Sample.ids.5}{Other metadata columns...}
+#'   \item{Sample.ids.6}{Other metadata columns...}
+#'   \item{Sample.ids.3.a}{Other metadata columns...}
+#'   \item{Sample.ids.4.a}{Other metadata columns...}
+#'   \item{Sample.ids.5.a}{Other metadata columns...}
+#'   \item{Sample.ids.6.a}{Other metadata columns...}
+#'   \item{Year}{Other metadata columns...}
+#'   \item{Month}{Other metadata columns...}
+#'   \item{Day}{Other metadata columns...}
+#'   \item{Plates}{Other metadata columns...}
+#'   \item{TSS}{Other metadata columns...}
+#'   \item{Center}{Other metadata columns...}
+#'   \item{Tissues}{Other metadata columns...}
+#'   \item{Cancer.type.a}{Other metadata columns...}
+#'   \item{Cancer.type}{Other metadata columns...}
+#' }
+#'
+#' @source \url{https://www.cancer.gov/tcga}
+"tcga.batch.info"
