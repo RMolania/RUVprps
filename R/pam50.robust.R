cc0daa56033bd84bb0c4dc610ad6278b72df856b
Ramyar Molania <ramyar.molania@gmail.com>
2025-09-12 14:36:47 -0400

    adding explanation for pam50 data

---
+++ R/pam50.robust.R
@@ -0,0 +1,20 @@
+#' PAM50 Robust List
+#'
+#' A list containing robust PAM50 gene signature objects used for
+#' molecular subtyping in prostate cancer analysis.
+#'
+#' @format A list with X elements:
+#' \describe{
+#'   \item{method.cor}{Description of element1}
+#'   \item{method.centroids}{Description of element2}
+#'   \item{rescale.q}{Description of element3}
+#'   \item{mins}{Description of element3}
+#'   \item{centroids}{Description of element3}
+#'   \item{centroids.map}{Description of element3}
+#'   ...
+#' }
+#'
+#' @examples
+#' data(pam50.robust)
+#' names(pam50.robust)
+"pam50.robust"
