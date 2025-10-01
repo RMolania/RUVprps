c39e2530852fd8a443dab13629a684edc9a0ef40
Ramyar Molania <ramyar.molania@gmail.com>
2025-10-01 12:39:38 -0400

    add explanation for the arguments

--- R/estimatePAM50.R
+++ R/estimatePAM50.R
@@ -2,25 +2,32 @@
 #'
 #' @author Ramyar Molania
 #'
-#' @param se.obj A SummarizedExperiment object.
-#' @param assay.names Character or character vector. The name(s) of the assay(s) in the `SummarizedExperiment` object.
-#' The default is set to 'all'.
-#' @param entrez.gene.id description
-#' @param probe description
-#' @param tissue.type description
-#' @param groups description
-#' @param apply.log description
-#' @param pseudo.count description
-#' @param normalization description
-#' @param regress.out.variables description
-#' @param regress.out.rle.med description
-#' @param out.put.name description
-#' @param check.se.obj description
-#' @param remove.na description
-#' @param verbose description
+#' @description
+#' This function estimates PAM50 molecular subtypes from RNA-seq or microarray data stored in a `SummarizedExperiment` object
+#' using the `genefu` package. It supports preprocessing steps such as normalization, log-transformation, and regression
+#' of unwanted variables.
+#'
+#' @param se.obj A `SummarizedExperiment` object containing gene expression data.
+#' @param assay.names Character or character vector. The name(s) of the assay(s) in `se.obj` to use. Default is `"all"`.
+#' @param entrez.gene.id Character. Column name in `rowData(se.obj)` containing Entrez gene IDs for mapping to PAM50 genes.
+#' @param probe Character. Column name in `rowData(se.obj)` containing probe or gene symbols if Entrez IDs are not used.
+#' @param tissue.type Character. Tissue type for samples (e.g., `"breast"`) to select the appropriate PAM50 reference.
+#' @param groups Character or NULL. Optional grouping variable(s) in `colData(se.obj)` to perform subtype estimation within groups.
+#' @param apply.log Logical. If `TRUE`, log2-transforms the assay data prior to subtype estimation. Default is `TRUE`.
+#' @param pseudo.count Numeric. Pseudo-count added to assay values before log2 transformation to avoid `-Inf`. Default is 1.
+#' @param normalization Character. Normalization method applied before subtype estimation (e.g., `"CPM"`, `"TMM"`). Default is `"CPM"`.
+#' @param regress.out.variables Character vector. Column names in `colData(se.obj)` representing unwanted variation variables to regress out. Default is `NULL`.
+#' @param regress.out.rle.med Logical. If `TRUE`, regress out the median relative log expression (RLE) per sample. Default is `FALSE`.
+#' @param out.put.name Character. Name to assign to the results stored in the `SummarizedExperiment` metadata.
+#' @param check.se.obj Logical. If `TRUE`, validates the structure of `se.obj` before analysis. Default is `TRUE`.
+#' @param remove.na Character. Determines how to handle missing values. Options: `"assays"`, `"sample.annotation"`, `"both"`, or `"none"`. Default is `"both"`.
+#' @param verbose Logical. If `TRUE`, prints progress messages during subtype estimation. Default is `TRUE`.
+#'
+#' @return A `SummarizedExperiment` object with PAM50 subtype assignments stored in the `colData`.
 #'
 #' @importFrom SummarizedExperiment colData
 #' @import genefu
+#' @export
 
 estimatePAM50 <- function(
         se.obj,
@@ -44,8 +51,16 @@ estimatePAM50 <- function(
     colnames(gene.annot)[egi] <- 'EntrezGene.ID'
     probe.col <- which(colnames(gene.annot) == probe)
     colnames(gene.annot)[probe.col] <- 'probe'
+
+    # Checking the assays ####
+    if (length(assay.names) == 1 && assay.names == 'all') {
+        assay.names <- factor(x = names(assays(se.obj)), levels = names(assays(se.obj)))
+    } else  assay.names <- factor(x = assay.names , levels = assay.names)
+    if(!sum(assay.names %in% names(assays(se.obj))) == length(assay.names)){
+        stop('The "assay.names" cannot be found in the SummarizedExperiment object.')
+    }
     pam50.genfu <- lapply(
-        names(assays(se.obj)),
+        levels(assay.names),
         function(x){
             expr.data <- preProcessData(
                 se.obj = se.obj,
@@ -71,7 +86,7 @@ estimatePAM50 <- function(
                 annot = gene.annot,
                 do.mapping = TRUE)
         })
-    names(pam50.genfu) <- paste0(names(assays(se.obj)), '.PAM50')
+    names(pam50.genfu) <- paste0(assay.names, '.PAM50')
     for(i in names(pam50.genfu)){
         SummarizedExperiment::colData(se.obj)[i] <- as.character(pam50.genfu[[i]]$subtype)
         index <- SummarizedExperiment::colData(se.obj)[[i]] == 'Normal'
@@ -87,6 +102,7 @@ estimatePAM50 <- function(
                 x = SummarizedExperiment::colData(se.obj)[ , i],
                 levels = c('Basal','Her2','LumA','LumB', 'Normal.like'))
         }
+        rm(i)
     }
     return(se.obj)
 }
