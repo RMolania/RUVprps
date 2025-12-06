7b05ca68a52b3d038b44ea43f84821e151bf8451
Ramyar Molania <ramyar.molania@gmail.com>
2025-12-06 17:16:58 -0500

    adding new functions to calculate residual opreation

--- src/fastCppFunctions.cpp
+++ src/fastCppFunctions.cpp
@@ -3,7 +3,7 @@
 // [[Rcpp::depends(RcppEigen)]]
 
 // [[Rcpp::export]]
-SEXP fastResidop(const Eigen::Map<Eigen::MatrixXd> A, Eigen::Map<Eigen::MatrixXd> B){
+SEXP fastResidopC1(const Eigen::Map<Eigen::MatrixXd> A, Eigen::Map<Eigen::MatrixXd> B){
   Eigen::MatrixXd Y1 = B.transpose() * B ;
   Eigen::MatrixXd Y2 = Y1.inverse();
   Eigen::MatrixXd Y3 = B * Y2;
@@ -14,11 +14,26 @@ SEXP fastResidop(const Eigen::Map<Eigen::MatrixXd> A, Eigen::Map<Eigen::MatrixXd
 }
 
 // [[Rcpp::export]]
-SEXP fastResidop2(const Eigen::Map<Eigen::MatrixXd> A, Eigen::Map<Eigen::MatrixXd> B){
+SEXP fastResidopC2(const Eigen::Map<Eigen::MatrixXd> A, Eigen::Map<Eigen::MatrixXd> B){
   Eigen::MatrixXd Y1 = B.transpose() * B ;
   Eigen::MatrixXd Y2 = A - B * Y1.inverse() * B.transpose() * A;
   return Rcpp::wrap(Y2);
 }
+
+
+// [[Rcpp::export]]
+SEXP fastResidopC1lQR(const Eigen::Map<Eigen::MatrixXd> A,
+                    const Eigen::Map<Eigen::MatrixXd> B) {
+
+  Eigen::HouseholderQR<Eigen::MatrixXd> qr(B);
+  Eigen::MatrixXd Q = qr.householderQ() * Eigen::MatrixXd::Identity(B.rows(), B.cols());
+
+  Eigen::MatrixXd QtA = Q.transpose() * A;
+  Eigen::MatrixXd result = A - Q * QtA;
+
+  return Rcpp::wrap(result);
+}
+
 
 // [[Rcpp::export]]
 SEXP matrixMult(const Eigen::Map<Eigen::MatrixXd> A, Eigen::Map<Eigen::MatrixXd> B){
